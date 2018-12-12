2dd077a00075d4e822886fc8602a9dc88126bddd
Kosuke Kito <blueberryjam09@gmail.com>
2018-12-12 17:40:17 +0900

    Add project

---
+++ flutter_image_viewer_sample/lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class ImageViewerSample extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Image Viewer Sample',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
